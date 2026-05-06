17993975d3b4d6b2afe37d568b82f3098d9f15fa
Muzammil <notmuzammil1@gmail.com>
2026-05-06 16:54:39 +0500

    Math Functions

---
+++ Math Functions/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int x = 9;
+    float PI = 3.14;
+    int y = -7;
+
+    // x = sqrt(x);
+    // x = pow(x, 2);
+    // PI = round(PI); round off to the closest integer
+    // PI = ceil(PI); round off to the next integer
+    // PI = floor(PI); round off to the previous integer
+    // y = abs(y);
+    // PI = log(PI);
+    // x = sin(x); takes value in radians
+    // x = cos(x);
+    // x = tan(x);
+
+    printf("%d\n", x);
+    printf("%.2f\n", PI);
+    printf("%d", y);
+
+    return 0;
+}
