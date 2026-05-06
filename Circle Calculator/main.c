4e75d47e6b4c5b02e95ea9cf8ff86948897f60c9
Muzammil <notmuzammil1@gmail.com>
2026-05-06 17:14:11 +0500

    Circle Calculator

---
+++ Circle Calculator/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double radius = 0.0;
+    double area = 0.0;
+    double surfaceArea = 0.0;
+    double volume = 0.0;
+    const double PI = 3.14;
+
+    printf("Enter the Radius: ");
+    scanf("%lf", &radius);
+
+    area = PI * pow(radius, 2);
+    printf("Area: %.2lf\n", area);
+
+    surfaceArea = 4 * PI * pow(radius, 2);
+    printf("Surface Area: %.2lf\n", surfaceArea);
+
+    volume = ( 4.0 / 3.0 ) * PI * pow(radius, 3);
+    printf("Volume: %.2lf\n", volume);
+
+    return  0;
+}
